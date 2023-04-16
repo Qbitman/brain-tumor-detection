979ae7174c04693d30ba7e713766d417d05dab5e
Chaitanya <chaitanyauge1@gmail.com>
2023-04-16 17:03:05 +0530

    added flask files

--- src/components/pages/Predict.js
+++ src/components/pages/Predict.js
@@ -6,12 +6,14 @@ export default function Predict() {
     <div className="predict">
 
         <form className="form-design">
+          <div className='UploadBox'>
         <h3>Prediction Model</h3>
         <label class="custom-file-upload">
             <input type="file"/>
             Upload .jpg
         </label>
           <button className="button button-black" type="submit">Submit</button>
+          </div>
         </form>
       </div>
   )
