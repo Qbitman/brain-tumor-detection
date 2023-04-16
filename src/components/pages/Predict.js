7b85bd44e719e0933d697eb129f151455a7d50d1
Chaitanya <chaitanyauge1@gmail.com>
2023-04-16 17:24:44 +0530

    added dependancies for model.json

--- src/components/pages/Predict.js
+++ src/components/pages/Predict.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.css';
+import * as tf from '@tensorflow/tfjs';
+
 
 export default function Predict() {
+  const [model, setModel] = useState(null);
+
+  async function loadModel() {
+    const loadedModel = await tf.loadLayersModel("https://raw.githubusercontent.com/Qbitman/brain-tumor-detection/main/model.json");
+    console.log("Model loaded");
+    setModel(loadedModel);
+  }
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    const imageFile = e.target.elements.image.files[0];
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const image = new Image();
+      image.src = event.target.result;
+      image.onload = async () => {
+        const tensor = tf.browser.fromPixels(image).resizeNearestNeighbor([224, 224]).toFloat().div(tf.scalar(255.0)).expandDims();
+        const prediction = await TumorDetection(model, tensor);
+        console.log(prediction);
+      };
+    };
+    reader.readAsDataURL(imageFile);
+  }
+
+  async function TumorDetection(model, image) {
+    const prediction = await model.predict(image).data();
+    return prediction;
+  }
+
   return (
     <div className="predict">
-
-        <form className="form-design">
-          <div className='UploadBox'>
-        <h3>Prediction Model</h3>
-        <label class="custom-file-upload">
-            <input type="file"/>
+      <form className="form-design" onSubmit={handleFormSubmit}>
+        <div className='UploadBox'>
+          <h3>Prediction Model</h3>
+          <label className="custom-file-upload">
+            <input type="file" name="image" />
             Upload .jpg
-        </label>
+          </label>
           <button className="button button-black" type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
+        </div>
+      </form>
+    </div>
   )
 }
