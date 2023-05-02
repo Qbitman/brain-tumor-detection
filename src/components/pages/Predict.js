862e7cfe98f7f250cdfea5568fbd58ae4c936a75
chaitanya <ChaitanyaSuresh.Uge@infineon.com>
2023-05-03 00:29:13 +0530

    resolved alt texts

--- src/components/pages/Predict.js
+++ src/components/pages/Predict.js
@@ -57,7 +57,7 @@ export default function Predict() {
         <h4>Prediction Output</h4>
         <div className='imageSize'>
         <div className='uploadedImage'>
-        {imageSrc && <img src={imageSrc} alt="Uploaded image" style={{ maxWidth: '100%', height: 'auto' }} />}
+        {imageSrc && <img src={imageSrc} alt="Uploaded" style={{ maxWidth: '100%', height: 'auto' }} />}
         </div>
         </div>
         <div className='outputText'>
